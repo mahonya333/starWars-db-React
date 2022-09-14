4a304776bef7e5c50553b96d515a2e8136cb50a2
mahonya333 <mahonya333@gmail.com>
2022-09-14 20:38:19 +0300

    Lesson 54: Error processing FETCH API

--- src/index.js
+++ src/index.js
@@ -1,11 +1,19 @@
 const getResource = async (url) => {
   const res = await fetch(url);
+
+  if (!res.ok) {
+    throw new Error(`Could not fetch ${url}` +`, received ${res.status}`)
+  }
+
   const body = await res.json();
 
   return body
 }
 
-getResource('https://swapi.dev/api/people/1/')
+getResource('https://swapi.dev/api/people/123123123/')
   .then((body) => {
     console.log(body);
   })
+.catch((err) => {
+  console.log(err);
+})
