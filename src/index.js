0fc465d5cea38a72bddf35d8a7a637de29454c7b
mahonya333 <mahonya333@gmail.com>
2022-09-14 20:27:51 +0300

    Lesson 53: Fetch API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const getResource = async (url) => {
+  const res = await fetch(url);
+  const body = await res.json();
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  return body
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+getResource('https://swapi.dev/api/people/1/')
+  .then((body) => {
+    console.log(body);
+  })
